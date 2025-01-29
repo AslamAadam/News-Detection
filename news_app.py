f350c3ea124871adc83a2fc25dabba132abc5aef
Aslam Adam <aslammarakkarakath.435@gmail.com>
2025-01-29 14:33:19 +0400

    news_app.py

--- news_app.py
+++ news_app.py
@@ -15,7 +15,7 @@ def main():
     )
     st.markdown(
         """
-        <h1 style='text-align: center; color: #4CAF50;'>
+        <h1 style='text-align: center; color: #ffffff;'>
         📰 News Detection App
         </h1>
          """,
